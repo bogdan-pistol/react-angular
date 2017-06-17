6e505c519d3adb01aafd6c94f608dbd621e8b73b
bogdan.pistol <pistol_bgz@yahoo.com>
2017-06-17 17:42:50 +0300

    hello world example

---
+++ main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+// React.createElement(component, props, ...children)
+ReactDOM.render(React.createElement(
+  'h1', //component
+  null, //props
+  'Hello, world!' //children
+), document.getElementById('my-app'));
+
