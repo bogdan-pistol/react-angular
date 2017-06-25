b19fd881e2dbc01175e0618463965a03929706c7
bogdan.pistol <pistol_bgz@yahoo.com>
2017-06-25 19:12:48 +0300

    added jsx with state

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 'use strict';
 
+//run npm install axios
+
+import axios from 'axios';
+
 //----------React with components
 
 class App extends React.Component {
 
     render() {
-       return <div className="main">
-           <Header/>
-           <Body/>
-           <Footer/>
-       </div>
+        return <div className="main">
+            <Header/>
+            <Body/>
+            <Footer/>
+        </div>
     }
 }
-
-
 
 class Header extends React.Component {
     render() {
@@ -45,7 +47,75 @@ class SidePanel extends React.Component {
 
 class Content extends React.Component {
     render() {
-        return <div className="content">Here is the main contents area</div>
+        return <TodoList />
+    }
+}
+
+const BASE_URL = 'https://jsonplaceholder.typicode.com';
+
+class TodoList extends React.Component {
+
+    constructor() {
+        super()
+        this.state = {todos: []};
+    }
+
+    componentWillMount() {
+        this.getToDos().then(
+            todos => this.setState({todos})
+        );
+    }
+
+    getToDos() {
+        const url = `${BASE_URL}/todos`;
+        return axios.get(url).then(response => response.data);
+    }
+
+    render() {
+        let {todos}= this.state;
+        return (
+            <div>
+                {todos &&
+
+                    todos.slice(0, 15).map((todo, index) => (
+                        <Todo key={index} id={todo.id} completed={todo.completed} title={todo.title} userId={todo.userId}/>
+                    ))
+
+                }
+
+            </div>
+        )
+    }
+}
+
+class Todo extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: props.id,
+            completed: props.completed,
+            title: props.title,
+            userId: props.userId
+        }
+    }
+
+    render() {
+        let {id, completed, title, userId}= this.state;
+
+        return (
+            <div>
+                <div>
+                    <span>id: {id}&nbsp;</span>
+                    <span>completed: {completed.toString()}&nbsp;</span>
+                </div>
+                <div>
+                    <span>Tile: {title}&nbsp;</span>
+                    <span>UserId: {userId}&nbsp;</span>
+                </div>
+                <hr />
+            </div>
+        )
     }
 }
 
